b7d67e402dcac9b09f8778a91c9c42c1f9d2b38a
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2024-01-31 01:15:10 -0300

    Desde 0

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Button from "@mui/material/Button"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>asda</h1>
+     <Button>adsad</Button>
+    </>
+  )
+}
+
+export default App
